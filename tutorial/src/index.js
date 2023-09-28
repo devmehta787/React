aadf26e0ff4dde6960bf8aa2f56c9a9d5c970284
devmehta787 <devmehta787@gmail.com>
2023-09-28 22:45:13 +0530

    props

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,12 +1,92 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
+
 
 // componentin react
-function Greeting() {
-  return <h2>My First Component</h2>;
-}
+// function Greeting() {
+//   return <h2>My First Component</h2>;
+// }
 
-const root =ReactDOM.createRoot(document.getElementById("root"));
+// const Greeting2 = () => {
+//     return React.createElement("h2", {}, "My Second Component");
+// };
 
-root.render(<Greeting />);
+
+// JSX
+// function Greeting() {
+//     return (
+//         <div>
+//             < h2 > My First Component</h2 >
+//             <h1>hello</h1>
+//             <Greeting2/>
+//         </div>
+//     );
+      
+// }
+
+// const Greeting2 = () => {
+//     return (
+//         <div>
+//             < h2 > My Second Component</h2 >
+//             <h1>hello</h1>
+//         </div>
+//     );
+// }
+
+// const root =ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<Greeting />);
+
+
+
+
+const BookList = () => {
+    return (
+        <section className="booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+};
+
+const Book = () => {
+    return (
+        <article className="book">
+            <Image />
+            <Title />
+            <Author />
+            <div>
+                {}
+            </div>
+        </article>
+    );
+};
+
+const Image = () => {
+    return (
+        <img
+            src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg"
+            alt=""
+        />
+    );
+};
+
+const Title = () => {
+    return <h1>Ikigai: The Japanese secret to a long and happy life</h1>;
+};
+
+const Author = () => {
+    return <h4>Héctor García</h4>;
+};
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
