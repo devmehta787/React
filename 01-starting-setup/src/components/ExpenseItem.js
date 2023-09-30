d66699d5630fd1488bf4b4392cb2c9c6b873841a
devmehta787 <devmehta787@gmail.com>
2023-10-01 01:23:56 +0530

    Composition Children props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,37 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./card";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    return (
-        <div className="expense-item">
-            <div>{ expenseDate.toISOString() }</div>
-            <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
-            </div>
-        </div>
-    )
+// function ExpenseItem({ title, amount, date }) { //Object destructuring syntax
+//    return (
+//     <div className="expense-item">
+//       <div>{date.toISOString()}</div>
+//       <div className="expense-item__description">
+//         <h2>{title}</h2>
+//         <div className="expense-item__price">$ {amount}</div>
+//       </div>
+//     </div>
+//   );
+// }
+function ExpenseItem(props) {
+  //   const month = props.date.toLocaleString("en-US", { month: "long" });
+  //   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  //   const year = props.date.getFullYear();
+  return (
+    <Card className="expense-item">
+      {/* <div>{props.date.toISOString()}</div> */}
+      {/* <div> 
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div> */}
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
