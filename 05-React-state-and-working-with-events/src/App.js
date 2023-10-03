6c5486e1ec07d76e0b3b2d14ddd0766f232dc0ce
devmehta787 <devmehta787@gmail.com>
2023-10-03 23:28:14 +0530

    05-react-state-and-working-with-events adding form inputs

--- 05-React-state-and-working-with-events/src/App.js
+++ 05-React-state-and-working-with-events/src/App.js
@@ -1,6 +1,7 @@
 // import ExpenseItem from "./components/ExpenseItem";
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -31,7 +32,8 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      {/* <h2>Let's get started!</h2> */}
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
