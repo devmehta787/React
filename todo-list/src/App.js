b3207f8358008bf1b383d146a5e2fcb7130d687f
devmehta787 <devmehta787@gmail.com>
2023-10-16 23:53:40 +0530

    todo-list app props

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,13 +1,49 @@
+import React, { useState } from 'react';
+
 import './App.css';
 import TodoForm from './TodoForm/TodoForm';
 import TodoListItem from './TodoListItem/TodoListItem';
 
+var todoList = [
+  {
+    id: 1,
+    text: 'Todo',
+    priority: 'Low',
+    date:new Date(2023, 10, 15),
+    isDone: false
+
+  },
+  {
+    id: 2,
+    text: 'Todo Item',
+    priority: 'Medium',
+    date: new Date(2023, 10, 15),
+    isDone: false
+  },
+  {
+    id: 3,
+    text: 'Todo Item',
+    priority: 'High',
+    date: new Date(2023, 10, 15),
+    isDone: false
+  }
+];
+
 function App() {
+  const [todo, setTodo] = useState(todoList);
+
+  const addTodo = (todoItem) => { 
+    setTodo((prevTodo) => { 
+      return [...prevTodo, todoItem];
+    });
+    console.log(todoItem);
+  }
+
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm />
-      <TodoListItem/>
+      <TodoForm onAddTodo={addTodo} />
+      <TodoListItem items={todo} />
     </div>
   );
 }
--- todo-list/src/TodoListItem/TodoListItem.js
+++ todo-list/src/TodoListItem/TodoListItem.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import './TodoListItem.css'
 
-const TodoListItem = () => {
+const TodoListItem = (props) => {
   const [isChecked, setIsChecked] = useState(false);
 
   const toggleCheck = () => {
@@ -10,15 +10,42 @@ const TodoListItem = () => {
   const deleteItem = () => { 
     console.log('delete item');
   }
+
+
+
+  let todoList = <p className='noTodoContent'>No todo found.</p>;
+
+  if (props.items.length > 0) { 
+    todoList = props.items.map((todo) => { 
+      return (
+        <div className='list-item' key={todo.id}>
+          <div className='list-item-text'>
+            <p>{todo.text}</p>
+          </div>
+          <div className='list-item-action'>
+            <div className='action-button'>{todo.date.toLocaleDateString()}</div>
+            <button className='action-button' onClick={deleteItem}>Delete</button>
+            <button className='action-button done' onClick={toggleCheck}>
+              {isChecked ? '✔️' : '❌'}
+            </button>
+          </div>
+        </div>
+      )
+    })
+  }
+
+
+
+
+
   return (
     <div className='list'>
-      <div className='list-item'>
+      {/* <div className='list-item'>
         <div className='list-item-text'>
           <p>Todo Item abjsh fhfv jkha afjdc  g hdff hhkfh gagjv hsdfh dhjb</p>
         </div>
         <div className='list-item-action'>
           <div className='action-button'>10/15/2023</div>
-          {/* <button className='action-button'>Edit</button> */}
           <button className='action-button'>Delete</button>
           <button className='action-button done' onClick={toggleCheck}>
             {isChecked ? '✔️' : '❌'}
@@ -33,7 +60,6 @@ const TodoListItem = () => {
         </div>
         <div className='list-item-action'>
           <div className='action-button'>10/15/2023</div>
-          {/* <button className='action-button'>Edit</button> */}
           <button className='action-button' onClick={deleteItem}>Delete</button>
           <button className='action-button done' onClick={toggleCheck}>
             {isChecked ? '✔️' : '❌'}
@@ -48,13 +74,14 @@ const TodoListItem = () => {
         </div>
         <div className='list-item-action'>
           <div className='action-button'>10/15/2023</div>
-          {/* <button className='action-button'>Edit</button> */}
           <button className='action-button'>Delete</button>
           <button className='action-button done' onClick={toggleCheck}>
             {isChecked ? '✔️' : '❌'}
           </button>
         </div>
-      </div>
+      </div> */}
+      {todoList}
+
     </div>
   )
 }
